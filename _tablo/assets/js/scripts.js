650adb2be2cf3b34d77602139c8ebc8be5184048
ofc76 <ofc76@mail.ru>
2021-12-16 21:13:13 +0200

    add _tablo

---
+++ _tablo/assets/js/scripts.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $('.top-content').backstretch("assets/img/backgrounds/tablo.jpg");
+    $('.single-feature-bg').backstretch("assets/img/backgrounds/tablo.jpg");
+    
+});
+
